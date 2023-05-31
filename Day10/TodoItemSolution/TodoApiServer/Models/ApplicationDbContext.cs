2b89919e16d53dc8dd31e3f63835f151aeb903a0
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-05-31 17:30:59 +0900

    커밋

---
+++ Day10/TodoItemSolution/TodoApiServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+// 1. DB 연동
+namespace TodoApiServer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // 생성자 마법사로 만들것
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+
+}
---
+++ Day10/TodoItemSolution/TodoApiServer/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApiServer.Models
+{
+    // DB 모델
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Title", TypeName="Varchar(100)")]
+        public string? Title { get; set; }
+        public DateTime? TodoDate { get; set; }
+        public bool? IsComplete { get; set; }
+    }
+}
