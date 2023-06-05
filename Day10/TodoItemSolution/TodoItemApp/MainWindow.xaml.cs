bc2fd52c5947911f287b27c751f52101cf1d9470
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-06-05 11:22:09 +0900

    커밋

--- Day10/TodoItemSolution/TodoItemApp/MainWindow.xaml.cs
+++ Day10/TodoItemSolution/TodoItemApp/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TodoItemApp.Models;
 
 namespace TodoItemApp
 {
@@ -28,6 +33,9 @@ namespace TodoItemApp
     public partial class MainWindow : MetroWindow
     {
         private List<DivCode> divCodes = new List<DivCode>();
+        HttpClient client = new HttpClient();
+        TodoItemsCollection todoItems = new TodoItemsCollection();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -37,10 +45,103 @@ namespace TodoItemApp
         {
             divCodes.Add(new DivCode { Key = "True", Value = "1" });
             divCodes.Add(new DivCode { Key = "False", Value = "0" });
-            CboIsComplete.DataContext = divCodes;
-            CboIsComplete.DisplayMemberPath = "Key";
+            CboIsComplete.ItemsSource = divCodes;
+            CboIsComplete.DisplayMemberPath = "Key";    // 콤보박스에 True/False 추가
+            // yyyy-MM-dd HH:mm:ss 날짜 포맷(오전/오후 포함)
+            DtpTodoDate.Culture = new CultureInfo("ko-KR");
 
-            DtpTodoDate.Culture = new System.Globalization.CultureInfo("ko-KR");
+            // RestApi 호출 시작    // 아래 두 문장은 다시 호출하면 안됨
+            client.BaseAddress = new System.Uri("https://localhost:7058/"); // RestAPI 서버 기본 URL
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            GetData();  // API 데이터 로드 메서드 호출
+
+
+        }
+
+        // RestAPI Get Method 호출
+        private async void GetData()
+        {
+            GrdTodoItems.ItemsSource = todoItems;   // 미리 바인딩
+
+            try // API 호출
+            {
+                // https://localhost:7058/api/TodoItems
+                HttpResponseMessage? response = await client.GetAsync("api/TodoItems");  //Get method 비동기 호출
+                response.EnsureSuccessStatusCode(); // 에러가 났으면 오류코드를 던진다(예외발생)
+
+                // 응답에서 List<TodoItem> 형식으로 읽어옴
+                var items = await response.Content.ReadAsAsync<IEnumerable<TodoItem>>();
+                todoItems.CopyFrom(items);
+            }
+            catch (Newtonsoft.Json.JsonException jEx)
+            {
+                await this.ShowMessageAsync("error", jEx.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+                {
+                    // 메시지 다이얼로그가 나타날 때와 사라질 때 애니메이션 효과
+                    AnimateShow = true,
+                    AnimateHide = true
+                });
+            }
+            catch (HttpRequestException ex)
+            {
+                await this.ShowMessageAsync("error", ex.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+                {
+                    AnimateShow = true,
+                    AnimateHide = true
+                });
+            }
+        }
+        private async void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var todoItem = new TodoItem()
+                {
+                    Id = 0,
+                    Title = TxtTitle.Text,
+                    TodoDate = ((DateTime)DtpTodoDate.SelectedDateTime).ToString("yyyy-MM-dd HH:mm:ss"),
+                    IsComplete = Int32.Parse((CboIsComplete.SelectedItem as DivCode).Value)
+                };
+
+                // Insert하라때는 POST메서드 사용
+                var response = await client.PostAsJsonAsync("api/TodoItems", todoItem);
+                response.EnsureSuccessStatusCode();
+
+                GetData();
+
+                TxtId.Text = TxtTitle.Text = string.Empty;
+                CboIsComplete.SelectedIndex = -1;
+            }
+            catch (Newtonsoft.Json.JsonException jEx)
+            {
+                await this.ShowMessageAsync("error", jEx.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+                {
+                    // 메시지 다이얼로그가 나타날 때와 사라질 때 애니메이션 효과
+                    AnimateShow = true,
+                    AnimateHide = true
+                });
+            }
+            catch (HttpRequestException ex)
+            {
+                await this.ShowMessageAsync("error", ex.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+                {
+                    AnimateShow = true,
+                    AnimateHide = true
+                });
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Day10/TodoItemSolution/TodoItemApp/Models/TodoItemsCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItemApp.Models
+{
+    public class TodoItemsCollection: ObservableCollection<TodoItem>
+    // ObservableCollection
+    //  ListView와 함께 사용하여  동적인 목록을 표시하고 변경 사항을 실시간으로 반영할 수 있습니다.                                  
+    //  해당 변경 사항을 감지하고 처리할 수 있음.
+    //  OnCollectionChanged: 컬렉션 변경 사항을 발생시키기 위해 사용                   
+    // IList<TodoItem>, List<TodoItem>
+    {
+        public void CopyFrom(IEnumerable<TodoItem> items)
+        {
+            this.Items.Clear(); // ObservableCollection<T> 자체가 Items 속성을 가지고 있음 모두 삭제
+
+            foreach (TodoItem item in items)
+            {
+                this.Items.Add(item);   // 하나씩 다시 추가
+            }
+            // 데이터바꼈어요(전부 초기화)!  컬렉션 변경 사항을 발생
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
